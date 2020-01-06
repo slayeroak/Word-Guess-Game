55ddb7bcf8a34a07c5d1ba146d07a33ed6157db7
slayeroak <roakley1@villanova.edu>
2020-01-06 15:21:12 -0500

    made variables from psuedo code in game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+    var gameTickerArry = ["aapl", "acb", "amzn", "amd", "ba", "baba", "bac", "bayry", "bynd", "cat", "cgc", "d", "dis",
+                            "etsy", "f", "fb", "ge", "goog", "hpq", "hmy", "ibm", "irdm","jnj", "jpm", "k", "keys", "lmt",
+                             "lulu", "msft", "mcd", "nflx", "nok", "qcom", "rad", "roku", "shop", "snap", "tlry", "tmus",
+                             "tsg", "tsla", "uber", "ua", "vs", "vz", "wmt", "work", "yelp", "yten", "znga", "zm",]
+
+    const maxGuess = 10
+    var pauseGame = false
+
+
+    // Game Variables
+
+    var guessedLetters = []
+    var guessingTicker = []
+    var  tickerMatch
+    var numGuess
+    var wins = 0
+
+
+
+})
