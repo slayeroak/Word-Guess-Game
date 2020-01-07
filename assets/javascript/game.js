e4bd71f5697c1b92176fa914c61bc5baf4a6a7e8
slayeroak <roakley1@villanova.edu>
2020-01-06 19:51:34 -0500

    function listen for key event, letter check function, char function, reset game function, reset guessing ticker, update display function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,10 +13,98 @@ $(document).ready(function() {
 
     var guessedLetters = []
     var guessingTicker = []
-    var  tickerMatch
+    var tickerMatch
     var numGuess
     var wins = 0
 
+
+    resetGame()
+
+    // Grabs key press and passes the event through the block
+    document.onkeypress = function(event) {
+        // makes sure the event is an alpha char
+        if (isAlpha(event.key) && !pauseGame) {
+            letterCheck(event.key.toUpperCase())
+        }
+    }
+
+    // Game Functions
+
+    function letterCheck(letter) {
+        var foundLetter = false
+        // var correctSound = document.createElement("audio")
+        // var incorrectSound = document.createElement("audio")
+        // correctSound.setAttribute("src", "")
+        // incorrectSound.setAttribute("src", "")
+
+        // search string for letter
+        for ( var i = 0; i < tickerMatch.length; i++)  {
+            if (letter === tickerMatch[i]) {
+                guessingTicker[i] = letter
+                foundLetter = true
+                // correctSound.play
+
+                // If guessing word matches random ticker
+                if (guessingTicker.join("") === tickerMatch) {
+                    // increment wins 
+                    wins++
+                    pauseGame = true
+                    updateDisplay()
+                    setTimeout(resetGame, 5000)
+                }
+            }
+        }
+
+        if (!foundLetter) {
+            // incorrectSound.play()
+            // check if incorrect guess is on the list
+            if (!guessedLetters.includes(letter)) {
+                guessedLetters.push(letter)
+                // subtract 1 from remaining guess
+                numGuess--
+            }
+            if(numGuess ===0) {
+                // display ticker before game reset
+                guessingTicker = tickerMatch.split()
+                pauseGame = true
+                setTimeout(resetGame, 5000)
+            }
+        }
+        updateDisplay()
+    }
+
+    // Checks if key pressed is A-Z a-z
+    function isAlpha (ch) {
+        return /^[A-Z]$/i.test(ch);
+    }
+
+    function resetGame() {
+        numGuess = maxGuess
+        pauseGame = false
+
+        // Get a new word
+        tickerMatch = gameTickerArry[Math.floor(Math.random() * gameTickerArry.length)].toUpperCase()
+        console.log(tickerMatch)
+
+        // Reset the guessing word
+        for (var i = 0; i < tickerMatch.length; i++) {
+            // space instead of underscore
+            if (tickerMatch[i] === " ") {
+                guessingTicker.push(" ")
+            } else {
+                guessingTicker.push("_")
+            }
+        }
+
+        updateDisplay()
+    }
+
+    function updateDisplay () {
+        document.getElementById("totalWins").innerText = wins
+        document.getElementById("currentTicker").innerText = guessingTicker.join("")
+        document.getElementById("guessesRemaining").innerText = numGuess
+        document.getElementById("guessedLetters").innerText = guessedLetters.join(" ")
+    }
 
 
 })
